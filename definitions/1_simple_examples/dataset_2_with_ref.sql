ee13c251105324126e042d0c370a8f6b3c363820
Guillaume-Henri Huon <ghh@dataform.co>
2019-02-13 09:02:02 +0000

    change

--- definitions/1_simple_examples/dataset_2_with_ref.sql
+++ definitions/1_simple_examples/dataset_2_with_ref.sql
@@ -1,8 +1,11 @@
 -- learn more on https://docs.dataform.co/guides/tables/
 
 --js type("table");
+/*js
+const foo = 1;
+*/
 
-SELECT country as country,
+SELECT ${foo} as country,
        ${mapping.country_group("country")} as country_group,
        device_type as device_type,
        sum(revenue) as revenue,
