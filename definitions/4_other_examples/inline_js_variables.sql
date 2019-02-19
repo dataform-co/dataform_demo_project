56fdcf3fef923e2d5f1893d43863338a223aef55
Guillaume-Henri Huon <ghh@dataform.co>
2019-02-19 17:12:21 +0000

    add new example

---
+++ definitions/4_other_examples/inline_js_variables.sql
@@ -0,0 +1,10 @@
+-- You can define js functions and variables within a SQL script like the following:
+
+/*js
+const foo = 1;
+*/
+
+--js const bar = 2;
+
+SELECT ${foo} as one,
+       ${bar} as two
