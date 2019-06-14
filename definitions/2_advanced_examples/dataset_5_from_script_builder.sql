7e64c0a0fe7f7b579812a26d2ed32780f6accffc
Dan Lee <danlee@Dans-MacBook-Pro.local>
2019-06-14 10:50:01 +0100

    first round of sqlx changes

--- definitions/2_advanced_examples/dataset_5_from_script_builder.sql
+++
@@ -1,6 +0,0 @@
--- learn more on https://docs.dataform.co/guides/includes/
-
-${script_builder.render_script(ref("dataset_1"),
-                               ["country", "device_type"],
-                               ["revenue", "pageviews", "sessions"]
-                               )}
